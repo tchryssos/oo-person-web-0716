57c12d2110afff51c2ca13ef310eb366286d4fca
Troy Chryssos <github email address>
2016-07-28 12:19:05 -0400

    created person class that tracks happiness and cleanliness, which are affected by things like bathing, working out, calling friends, and the topics of conversation.

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,72 @@
 # your code goes here
+require 'pry'
+
+
+class Person
+  attr_accessor :bank_account
+  attr_reader :name, :happiness, :hygiene
+
+  def initialize(name)
+    @name=name
+    @bank_account = 25
+    @happiness=8
+    @hygiene=8
+  end
+
+  def happiness=(happiness)
+    @happiness=happiness
+    @happiness=[[10,@happiness].min,0].max
+  end
+
+  def hygiene=(hygiene_score)
+    @hygiene=hygiene_score
+    @hygiene=10 if hygiene > 10
+    @hygiene=0 if hygiene < 0 
+  end
+
+  def happy?
+    @happiness > 7 ? true : false
+  end
+
+  def clean?
+    @hygiene > 7 ? true : false
+  end
+
+  def get_paid(salary)
+    @bank_account+=salary
+    "all about the benjamins"
+  end
+
+  def take_bath
+    self.hygiene=(@hygiene+4)
+    "♪ Rub-a-dub just relaxing in the tub ♫"
+  end
+
+  def work_out
+    self.hygiene=(@hygiene-3)
+    self.happiness=(@happiness+2)
+    "♪ another one bites the dust ♫"
+  end
+
+  def call_friend(person)
+    self.happiness=(@happiness+3)
+    person.happiness=(person.happiness+3)
+    "Hi #{person.name}! It's #{self.name}. How are you?"
+  end
+
+  def start_conversation(person, topic)
+    if topic == "politics"
+      self.happiness=(@happiness-2)
+      person.happiness=(person.happiness-2)
+      "blah blah partisan blah lobbyist"
+    elsif topic == "weather"
+      self.happiness=(@happiness+1)
+      person.happiness=(person.happiness+1)
+      "blah blah sun blah rain"
+    else 
+      "blah blah blah blah blah"
+    end
+  end
+
+
+end
